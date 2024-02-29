a1acba5ac9357cc6424047414846516cb1ee5aed
Ming Du <ming.du101@gmail.com>
2024-02-29 12:14:53 -0600

    Removed unused dependencies

--- generic_trainer/trainer.py
+++ generic_trainer/trainer.py
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import matplotlib
-import pandas as pd
 
 from .configs import *
 from .util import *
